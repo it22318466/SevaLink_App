9b50a5435c12049d6949d8d78249a2f72157690e
ramindianusari <ramindi357@gmail.com>
2026-06-20 19:31:02 +0530

    Resolve profile picture loading issues by rewriting backend URLs

--- sevalink-backend/src/test/java/com/sevalink/sevalinkbackend/SevalinkBackendApplicationTests.java
+++ sevalink-backend/src/test/java/com/sevalink/sevalinkbackend/SevalinkBackendApplicationTests.java
@@ -1,13 +1,21 @@
 package com.sevalink.sevalinkbackend;
 
+import com.sevalink.sevalinkbackend.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SevalinkBackendApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     void contextLoads() {
+        userRepository.findAll().forEach(user -> {
+            System.out.println("USER_DEBUG: ID=" + user.getId() + ", Name=" + user.getFullName() + ", Email=" + user.getEmail() + ", ProfileImage=" + user.getProfileImageUrl() + ", Location=" + user.getLocation());
+        });
     }
 
 }
